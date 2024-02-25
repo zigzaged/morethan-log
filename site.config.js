05f034dad9e9a8250d4623e1f94714e020cc8d4d
zigzaged <95273317+zigzaged@users.noreply.github.com>
2024-02-25 17:37:08 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Heejun Kang",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Operation Manager",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
